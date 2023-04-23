2316dbfebaa8382d3fc53ad2dc29272aedae5b01
Juliana de Oliveira <julianajesusoliveira.jjo@gmail.com>
2023-04-23 15:02:52 -0300

    Stack's organization

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -1,21 +1,32 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ECommerceAwsStack } from '../lib/e_commerce_aws-stack';
+import { ProductsAppStack } from '../lib/productsApp-stack';
+import { ECommerceApiStack } from '../lib/ecommerceApi-stack';
 
 const app = new cdk.App();
-new ECommerceAwsStack(app, 'ECommerceAwsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const env: cdk.Environment = {
+  account: "349955741731",
+  region: "us-east-1"
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+//as tags são importantes para controlar os custos de cada time que criará recursos no projeto
+const tags = {
+  cost: "ECommerce",
+  team: "SiecolaCode"
+}
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
+  tags: tags,
+  env: env
+})
+
+const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", {
+  productsFetchHandler: productsAppStack.productsFetchHandler,
+  tags: tags,
+  env: env
+})
+// deve-se ter um entendimento de qual stack que deve ser criada primeiro, para não interferir em alguma dependência que tenham uma com a outra
+// abaixo deixamos claro que a stack eCommerceApiStack depende da productsAppStack, então esta deverá ser criada primeiro e não o contrário
+eCommerceApiStack.addDependency(productsAppStack)
