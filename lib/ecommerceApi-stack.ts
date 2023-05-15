dcace80fc91269ee637fa6a438d6817e109b2cc3
Juliana de Oliveira <julianajesusoliveira.jjo@gmail.com>
2023-05-15 07:21:28 -0300

    DynamoDB table creation and add operations in the api gateway

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -7,6 +7,7 @@ import { Construct } from "constructs"
 //quando cria a classe ECommerceApiStack, passa as propriedade da interface abaixo, para que a classe tenha acesso às informações de produtos 
 interface ECommerceApiStackProps extends cdk.StackProps {
    productsFetchHandler: lambdaNodeJS.NodejsFunction
+   productsAdminHandler: lambdaNodeJS.NodejsFunction
 }
 
 //Já há um padrão de classe e construtor
@@ -22,7 +23,6 @@ export class ECommerceApiStack extends cdk.Stack {
       const api = new apigateway.RestApi(this, "ECommerceApi", {
          //o nome abaixo é o que aparecerá no console do api gateway
          restApiName: "ECommerceApi",
-         cloudWatchRole: true,
          deployOptions: {
             accessLogDestination: new apigateway.LogGroupLogDestination(logGroup), //aonde eu vou gerar os logs
             //abaixo está o formato da geração dos logs
@@ -40,11 +40,27 @@ export class ECommerceApiStack extends cdk.Stack {
          }
       })
 
-      //integração para representar como o api gateway invocará aquela função (productsFetchHandler)
+      //Integração para representar como o api gateway invocará a função productsFetchHandler
       const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
 
       // "/products"
       const productsResource = api.root.addResource("products")
       productsResource.addMethod("GET", productsFetchIntegration)
+
+      // GET /products/{id}
+      const productIdResource = productsResource.addResource("{id}")
+      productIdResource.addMethod("GET", productsFetchIntegration)
+
+      //Integração para representar como o api gateway invocará a função productsAdminHandler
+      const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
+
+      // POST /products
+      productsResource.addMethod("POST", productsAdminIntegration)
+
+      // PUT /products/{id}
+      productIdResource.addMethod("PUT", productsAdminIntegration)
+
+      // DELETE /products/{id}
+      productIdResource.addMethod("DELETE", productsAdminIntegration)
    }
 }
--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -6,16 +6,39 @@ import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
 
 //https://docs.aws.amazon.com/cdk/api/v2/docs/aws-construct-library.html
 import * as cdk from "aws-cdk-lib"
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
+import * as ssm from "aws-cdk-lib/aws-ssm"
 
 import { Construct } from "constructs"
 
 export class ProductsAppStack extends cdk.Stack {
-   readonly productsFetchHandler: lambdaNodeJS.NodejsFunction 
-   //atributo readonly que será utilizado depois na chamada do api gateway
+   //Abaixo, as permissões:
+   readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+   readonly productsAdminHandler: lambdaNodeJS.NodejsFunction
+   readonly productsDdb: dynamodb.Table
 
    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props)
 
+      //Criação da tabela:
+      this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
+         tableName: "products",
+         //O padrão para removalPolicy qnd a stacké destruída é de manter (RETAIN)
+         //Aqui deixamos destroy pra caso quisermos iniciar o projeto do zero em algum momento
+         removalPolicy: cdk.RemovalPolicy.DESTROY,
+         partitionKey: {
+            name: "id",
+            type: dynamodb.AttributeType.STRING
+         },
+         billingMode: dynamodb.BillingMode.PROVISIONED,
+         readCapacity: 1, //O padrão é 5
+         writeCapacity: 1 //O padrão é 5
+      })
+
+      //Products Layer
+      const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
+      const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
+      
       //criando a lambda através do atributo de classe. 
       //usa o thi pra pegar as propriedades na qual a função está inserida
       this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, 
@@ -24,11 +47,35 @@ export class ProductsAppStack extends cdk.Stack {
             entry: "lambda/products/productsFetchFunction.ts", //caminho de onde estará o método
             handler: "handler",
             memorySize: 128, //quantidade de memória que será alocada para a função ser executada
-            timeout: cdk.Duration.seconds(5), //tempo máximo que a função será executada
+            timeout: cdk.Duration.seconds(5),
+            bundling: {
+               minify: true,
+               sourceMap: false               
+            },            
+            environment: {
+               PRODUCTS_DDB: this.productsDdb.tableName
+            }, 
+            layers: [productsLayer]
+         })
+      //Abaixo, a permissão para que a função acesse a tabela 
+      this.productsDdb.grantReadData(this.productsFetchHandler)
+
+      this.productsAdminHandler = new lambdaNodeJS.NodejsFunction(this, 
+         "ProductsAdminFunction", {
+            functionName: "ProductsAdminFunction",
+            entry: "lambda/products/productsAdminFunction.ts",
+            handler: "handler",
+            memorySize: 128,
+            timeout: cdk.Duration.seconds(5),
             bundling: {
                minify: true, //pega o código e transforma ele no mais enxuto possível (config em tempo de montagem da função lambda)
                sourceMap: false //desliga a geração de mapas para fazer debug              
             },            
-         })
+            environment: {
+               PRODUCTS_DDB: this.productsDdb.tableName
+            },
+            layers: [productsLayer]
+         }) 
+      this.productsDdb.grantWriteData(this.productsAdminHandler)
    }
 }
